ec3b8286bae445f1f360a83cab885f09934dcde1
hesamoon <he3am.jsm@mail.com>
2025-05-25 21:06:35 +0330

    blure screen when focus

--- components/DisableInteractions.tsx
+++ components/DisableInteractions.tsx
@@ -1,7 +1,7 @@
 // app/components/DisableInteractions.tsx
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function DisableInteractions() {
   useEffect(() => {
@@ -10,13 +10,21 @@ export default function DisableInteractions() {
     const disableSelect = (e: Event) => e.preventDefault();
 
     // document.addEventListener('contextmenu', disableRightClick);
-    document.addEventListener('copy', disableCopy);
-    document.addEventListener('selectstart', disableSelect);
+    document.addEventListener("copy", disableCopy);
+    document.addEventListener("selectstart", disableSelect);
+
+    const handleBlur = () => document.body.classList.add("blurred");
+    const handleFocus = () => document.body.classList.remove("blurred");
+    
+    window.addEventListener("blur", handleBlur);
+    window.addEventListener("focus", handleFocus);
 
     return () => {
       // document.removeEventListener('contextmenu', disableRightClick);
-      document.removeEventListener('copy', disableCopy);
-      document.removeEventListener('selectstart', disableSelect);
+      document.removeEventListener("copy", disableCopy);
+      document.removeEventListener("selectstart", disableSelect);
+      window.removeEventListener("blur", handleBlur);
+      window.removeEventListener("focus", handleFocus);
     };
   }, []);
 
