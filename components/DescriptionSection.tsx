8cdcae9cb65b38209601ae3d8ce83e1307dea181
hesamoon <he3am.jsm@mail.com>
2025-11-01 00:10:36 +0330

    show schedule for offline courses

--- components/DescriptionSection.tsx
+++ components/DescriptionSection.tsx
@@ -8,12 +8,14 @@ function DescriptionSection({
   prerequisites,
   audience,
   what_you_learn,
+  presentation_type,
   type,
 }: {
   about: string;
   prerequisites: { text: string }[];
   audience: { text: string }[];
   what_you_learn: { text: string }[];
+  presentation_type: string;
   type: string;
 }) {
   return (
@@ -29,79 +31,83 @@ function DescriptionSection({
       </div>
 
       {/* some important stuff */}
-      {type === "program" && <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {/* What are the prerequisites? */}
-        <div className="space-y-3">
-          <div className="flex items-center gap-2">
-            <Image src="/data.svg" alt="data" width={20} height={20} />
-            <h3 className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
-              What are the prerequisites?
-            </h3>
+      {type === "program" && (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* What are the prerequisites? */}
+          <div className="space-y-3">
+            <div className="flex items-center gap-2">
+              <Image src="/data.svg" alt="data" width={20} height={20} />
+              <h3 className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
+                What are the prerequisites?
+              </h3>
+            </div>
+
+            <ul className="space-y-1 mobile-body-large md:body-large text-txt-on-surface-secondary-light">
+              {prerequisites?.map((item) => (
+                <li key={item.text} className="flex items-center gap-2">
+                  <div className="w-2 h-2 rounded-full bg-txt-on-surface-secondary-light" />
+                  {item.text}
+                </li>
+              ))}
+            </ul>
           </div>
 
-          <ul className="space-y-1 mobile-body-large md:body-large text-txt-on-surface-secondary-light">
-            {prerequisites?.map((item) => (
-              <li key={item.text} className="flex items-center gap-2">
-                <div className="w-2 h-2 rounded-full bg-txt-on-surface-secondary-light" />
-                {item.text}
-              </li>
-            ))}
-          </ul>
-        </div>
+          {/* Who is the audience? */}
+          <div className="space-y-3">
+            <div className="flex items-center gap-2">
+              <Image src="/people.svg" alt="people" width={20} height={20} />
+              <h3 className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
+                Who is the audience?
+              </h3>
+            </div>
 
-        {/* Who is the audience? */}
-        <div className="space-y-3">
-          <div className="flex items-center gap-2">
-            <Image src="/people.svg" alt="people" width={20} height={20} />
-            <h3 className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
-              Who is the audience?
-            </h3>
+            <ul className="space-y-1 mobile-body-large md:body-large text-txt-on-surface-secondary-light">
+              {audience?.map((item) => (
+                <li key={item.text} className="flex items-center gap-2">
+                  <div className="w-2 h-2 rounded-full bg-txt-on-surface-secondary-light" />
+                  {item.text}
+                </li>
+              ))}
+            </ul>
           </div>
-
-          <ul className="space-y-1 mobile-body-large md:body-large text-txt-on-surface-secondary-light">
-            {audience?.map((item) => (
-              <li key={item.text} className="flex items-center gap-2">
-                <div className="w-2 h-2 rounded-full bg-txt-on-surface-secondary-light" />
-                {item.text}
-              </li>
-            ))}
-          </ul>
         </div>
-      </div>}
+      )}
 
       {/* what you'll learn */}
-      {type === "program" && <div className="space-y-3">
-        {/* title */}
-        <div className="flex items-center gap-[9px]">
-          <Image src="/verify.svg" alt="verify" width={20} height={20} />
-          <h4 className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
-            What you&apos;ll learn?
-          </h4>
-        </div>
+      {type === "program" && (
+        <div className="space-y-3">
+          {/* title */}
+          <div className="flex items-center gap-[9px]">
+            <Image src="/verify.svg" alt="verify" width={20} height={20} />
+            <h4 className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
+              What you&apos;ll learn?
+            </h4>
+          </div>
 
-        {/* list of what you'll learn */}
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-          {what_you_learn?.map((item, index) => (
-            <div key={index} className="flex items-start gap-4 py-2.5">
-              <h4 className="mobile-display-small md:display-small text-primary-tints-70">
-                {index + 1}
-              </h4>
-              <div className="flex flex-col gap-2">
-                <span className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
-                  You Learn
-                </span>
+          {/* list of what you'll learn */}
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+            {what_you_learn?.map((item, index) => (
+              <div key={index} className="flex items-start gap-4 py-2.5">
+                <h4 className="mobile-display-small md:display-small text-primary-tints-70">
+                  {index + 1}
+                </h4>
+                <div className="flex flex-col gap-2">
+                  <span className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
+                    You Learn
+                  </span>
 
-                <span className="mobile-body-medium md:body-medium text-txt-on-surface-secondary-light">
-                  {item.text}
-                </span>
+                  <span className="mobile-body-medium md:body-medium text-txt-on-surface-secondary-light">
+                    {item.text}
+                  </span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>}
+      )}
 
       {/* schedule */}
-      {type === "program" && (
+      {presentation_type === "offline" && (
         <div className="space-y-3">
           {/* title */}
           <h4 className="mobile-title-medium md:title-medium text-txt-on-surface-secondary-light">
